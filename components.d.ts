18c9102f3ac444017fbfdac03621cd1553ed8f1a
fangyaohui <2944163240@qq.com>
2024-08-03 10:06:29 +0800

    导航栏封装

--- components.d.ts
+++ components.d.ts
@@ -13,6 +13,7 @@ declare module 'vue' {
     IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
     IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
     IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
+    NavigationBar: typeof import('./src/components/NavigationBar.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     TheWelcome: typeof import('./src/components/TheWelcome.vue')['default']
